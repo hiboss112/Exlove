368e3a4742b43f352aaaebdbf9a8c510dd4bf067
hiboss112 <66228324a@gmail.com>
2025-03-23 01:00:05 +0900

    Update reg.bat

--- reg.bat
+++ reg.bat
@@ -1,42 +1,61 @@
 @echo off
 setlocal enabledelayedexpansion
 
-echo === Exlove 레지스트리 자동 설정 프로그램 ===
+echo === Exlove 레지스트리 설정 프로그램 ===
 echo.
 
 :: 관리자 권한 확인
-net session >nul 2>&1
-if %errorLevel% neq 0 (
-    echo 관리자 권한이 필요합니다.
-    echo 스크립트를 관리자 권한으로 다시 실행해주세요.
+>nul 2>&1 "%SYSTEMROOT%\system32\cacls.exe" "%SYSTEMROOT%\system32\config\system"
+if '%errorlevel%' NEQ '0' (
+    echo 관리자 권한으로 실행해주세요!
+    echo 파일 우클릭 후 "관리자 권한으로 실행" 선택
     pause
     exit /b 1
 )
 
-:: 현재 시간과 유저는 고정값 사용
-set "FIXED_TIME=2025-03-22 15:51:22"
-set "FIXED_USER=hiboss112"
+echo 레지스트리 초기화 중...
 
-echo 레지스트리 설정을 적용합니다...
+:: 먼저 HKCU 설정 (사용자별 설정)
+echo HKCU 설정 중...
+reg add "HKCU\SOFTWARE\idomnet" /f
+reg add "HKCU\SOFTWARE\idomnet\Exlove" /f
+reg add "HKCU\SOFTWARE\idomnet\Exlove" /v "CompanyName" /t REG_SZ /d "idomnet" /f
+reg add "HKCU\SOFTWARE\idomnet\Exlove" /v "GameName" /t REG_SZ /d "Exlove" /f
+reg add "HKCU\SOFTWARE\idomnet\Exlove" /v "Version" /t REG_SZ /d "1.0" /f
 
-:: HKEY_LOCAL_MACHINE 설정
+:: 그 다음 HKLM 설정 (시스템 전역 설정)
+echo HKLM 설정 중...
+reg add "HKLM\SOFTWARE\idomnet" /f
+reg add "HKLM\SOFTWARE\idomnet\Exlove" /f
 reg add "HKLM\SOFTWARE\idomnet\Exlove" /v "CompanyName" /t REG_SZ /d "idomnet" /f
 reg add "HKLM\SOFTWARE\idomnet\Exlove" /v "GameName" /t REG_SZ /d "Exlove" /f
 reg add "HKLM\SOFTWARE\idomnet\Exlove" /v "Version" /t REG_SZ /d "1.0" /f
 
-:: WOW6432Node 설정
+:: 마지막으로 WOW6432Node 설정
+echo WOW6432Node 설정 중...
+reg add "HKLM\SOFTWARE\WOW6432Node\idomnet" /f
+reg add "HKLM\SOFTWARE\WOW6432Node\idomnet\Exlove" /f
 reg add "HKLM\SOFTWARE\WOW6432Node\idomnet\Exlove" /v "CompanyName" /t REG_SZ /d "idomnet" /f
 reg add "HKLM\SOFTWARE\WOW6432Node\idomnet\Exlove" /v "GameName" /t REG_SZ /d "Exlove" /f
 reg add "HKLM\SOFTWARE\WOW6432Node\idomnet\Exlove" /v "Version" /t REG_SZ /d "1.0" /f
 
-:: HKEY_CURRENT_USER 설정 (시간과 유저 정보 포함)
-reg add "HKCU\SOFTWARE\idomnet\Exlove" /v "CompanyName" /t REG_SZ /d "idomnet" /f
-reg add "HKCU\SOFTWARE\idomnet\Exlove" /v "GameName" /t REG_SZ /d "Exlove" /f
-reg add "HKCU\SOFTWARE\idomnet\Exlove" /v "Version" /t REG_SZ /d "1.0" /f
-reg add "HKCU\SOFTWARE\idomnet\Exlove" /v "Username" /t REG_SZ /d "%FIXED_USER%" /f
-reg add "HKCU\SOFTWARE\idomnet\Exlove" /v "LastLoginTime" /t REG_SZ /d "%FIXED_TIME%" /f
+:: 설정 확인
+echo 레지스트리 확인 중...
+reg query "HKCU\SOFTWARE\idomnet\Exlove" >nul 2>&1
+if errorlevel 1 (
+    echo HKCU 레지스트리 설정 실패!
+    pause
+    exit /b 1
+)
+
+reg query "HKLM\SOFTWARE\idomnet\Exlove" >nul 2>&1
+if errorlevel 1 (
+    echo HKLM 레지스트리 설정 실패!
+    pause
+    exit /b 1
+)
 
 echo.
-echo 레지스트리 설정이 완료되었습니다.
+echo 레지스트리 설정 완료!
 echo 게임을 실행해주세요.
 pause
